ac1a5b673c40b22aa9805828a9bc55518233c0d8
mazola2k <mazsola2k@modernhackers.com>
2020-01-26 20:35:45 +0100

    Arrow SoCKit

--- sys/sys_analog.tcl
+++ sys/sys_analog.tcl
@@ -16,39 +16,39 @@ set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to SDIO_CMD
 #============================================================
 # VGA
 #============================================================
-set_location_assignment PIN_A13 -to VGA_R[0]
-set_location_assignment PIN_C13 -to VGA_R[1]
-set_location_assignment PIN_E13 -to VGA_R[2]
-set_location_assignment PIN_B12 -to VGA_R[3]
-set_location_assignment PIN_C12 -to VGA_R[4]
-set_location_assignment PIN_D12 -to VGA_R[5]
-set_location_assignment PIN_E12 -to VGA_R[6]
-set_location_assignment PIN_F13 -to VGA_R[7]
+set_location_assignment PIN_AG5 -to VGA_R[0]
+set_location_assignment PIN_AA12 -to VGA_R[1]
+set_location_assignment PIN_AB12 -to VGA_R[2]
+set_location_assignment PIN_AF6 -to VGA_R[3]
+set_location_assignment PIN_AG6 -to VGA_R[4]
+set_location_assignment PIN_AJ2 -to VGA_R[5]
+set_location_assignment PIN_AH5 -to VGA_R[6]
+set_location_assignment PIN_AJ1 -to VGA_R[7]
 
-set_location_assignment PIN_J9 -to VGA_G[0]
-set_location_assignment PIN_J10 -to VGA_G[1]
-set_location_assignment PIN_H12 -to VGA_G[2]
-set_location_assignment PIN_G10 -to VGA_G[3]
-set_location_assignment PIN_G11 -to VGA_G[4]
-set_location_assignment PIN_G12 -to VGA_G[5]
-set_location_assignment PIN_F11 -to VGA_G[6]
-set_location_assignment PIN_E11 -to VGA_G[7]
+set_location_assignment PIN_Y21 -to VGA_G[0]
+set_location_assignment PIN_AA25 -to VGA_G[1]
+set_location_assignment PIN_AB26 -to VGA_G[2]
+set_location_assignment PIN_AB22 -to VGA_G[3]
+set_location_assignment PIN_AB23 -to VGA_G[4]
+set_location_assignment PIN_AA24 -to VGA_G[5]
+set_location_assignment PIN_AB25 -to VGA_G[6]
+set_location_assignment PIN_AE27 -to VGA_G[7]
 
-set_location_assignment PIN_B13 -to VGA_B[0]
-set_location_assignment PIN_G13 -to VGA_B[1]
-set_location_assignment PIN_H13 -to VGA_B[2]
-set_location_assignment PIN_F14 -to VGA_B[3]
-set_location_assignment PIN_H14 -to VGA_B[4]
-set_location_assignment PIN_F15 -to VGA_B[5]
-set_location_assignment PIN_G15 -to VGA_B[6]
-set_location_assignment PIN_J14 -to VGA_B[7]
+set_location_assignment PIN_AE28 -to VGA_B[0]
+set_location_assignment PIN_Y23 -to VGA_B[1]
+set_location_assignment PIN_Y24 -to VGA_B[2]
+set_location_assignment PIN_AG28 -to VGA_B[3]
+set_location_assignment PIN_AF28 -to VGA_B[4]
+set_location_assignment PIN_V23 -to VGA_B[5]
+set_location_assignment PIN_W24 -to VGA_B[6]
+set_location_assignment PIN_AF29 -to VGA_B[7]
 
-set_location_assignment PIN_B11 -to VGA_HS
-set_location_assignment PIN_D11 -to VGA_VS
+set_location_assignment PIN_AD12 -to VGA_HS
+set_location_assignment PIN_AC12 -to VGA_VS
 
-set_location_assignment PIN_C10 -to VGA_SYNC_N
-set_location_assignment PIN_F10 -to VGA_BLANK_N
-set_location_assignment PIN_A11 -to VGA_CLK
+set_location_assignment PIN_AG2 -to VGA_SYNC_N
+set_location_assignment PIN_AH3 -to VGA_BLANK_N
+set_location_assignment PIN_W20 -to VGA_CLK
 
 #set_location_assignment PIN_AH27 -to VGA_EN
 set_instance_assignment -name WEAK_PULL_UP_RESISTOR ON -to VGA_EN
@@ -65,15 +65,15 @@ set_instance_assignment -name CURRENT_STRENGTH_NEW 8MA -to VGA_*
 
 #set_location_assignment PIN_AC24 -to AUDIO_L
 
-set_location_assignment PIN_K7 -to AUD_ADCDAT
-set_location_assignment PIN_K8 -to AUD_ADCLRCK
-set_location_assignment PIN_H7 -to AUD_BCLK
-set_location_assignment PIN_J7 -to AUD_DACDAT
-set_location_assignment PIN_H8 -to AUD_DACLRCK
-set_location_assignment PIN_G7 -to AUD_XCK
+set_location_assignment PIN_AC27 -to AUD_ADCDAT
+set_location_assignment PIN_AG30 -to AUD_ADCLRCK
+set_location_assignment PIN_AE7 -to AUD_BCLK
+set_location_assignment PIN_AG3 -to AUD_DACDAT
+set_location_assignment PIN_AH4 -to AUD_DACLRCK
+set_location_assignment PIN_AC9 -to AUD_XCK
 
-set_location_assignment PIN_J12 -to I2C_SCLK
-set_location_assignment PIN_K12 -to I2C_SDAT
+set_location_assignment PIN_AH30 -to I2C_SCLK
+set_location_assignment PIN_AF30 -to I2C_SDAT
 
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to AUD_*
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to I2C_*
@@ -83,13 +83,13 @@ set_instance_assignment -name CURRENT_STRENGTH_NEW 8MA -to AUDIO_*
 #============================================================
 # I/O #1
 #============================================================
-set_location_assignment PIN_V17 -to LED_USER
-set_location_assignment PIN_V16 -to LED_HDD
-set_location_assignment PIN_W16 -to LED_POWER
+set_location_assignment PIN_AE11 -to LED_USER
+set_location_assignment PIN_AF10 -to LED_HDD
+set_location_assignment PIN_AD10 -to LED_POWER
 
-set_location_assignment PIN_AA15 -to BTN_USER
-set_location_assignment PIN_W15 -to BTN_OSD
-set_location_assignment PIN_Y16 -to BTN_RESET
+set_location_assignment PIN_AD9 -to BTN_USER
+set_location_assignment PIN_AE9 -to BTN_OSD
+set_location_assignment PIN_AE12 -to BTN_RESET
 
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to LED_*
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to BTN_*
